7ba741b497cb12c4845558b993f43906194547f0
Kamal <panapakam.kamaleshwar@gmail.com>
2016-04-28 03:03:28 -0400

    Removed unnecessary spaces.

--- app/src/main/java/com/restws/cync/kamal/cync/BackgroundWebServerRequest.java
+++ app/src/main/java/com/restws/cync/kamal/cync/BackgroundWebServerRequest.java
@@ -10,33 +10,28 @@ import com.restws.cync.kamal.cync.data.CyncDBContract;
 import com.restws.cync.kamal.cync.data.CyncDBHelper;
 
 public class BackgroundWebServerRequest
-        extends AsyncTask<Void, Void, Void>
-{
+        extends AsyncTask<Void, Void, Void> {
     private static final SQLiteQueryBuilder sRegisteredContacts = new SQLiteQueryBuilder();
     private Context mContext;
     private String updatedNumber = null;
     private String oldContact;
 
-    static
-    {
-        sRegisteredContacts.setTables(CyncDBContract.ServerContactsEntry.TABLE_NAME+" LEFT JOIN " +
-                CyncDBContract.ContactsEntry.TABLE_NAME +" ON "+
-                CyncDBContract.ServerContactsEntry.COLUMN_NUMBER +" = "+ CyncDBContract.ContactsEntry.COLUMN_NUMBER);
+    static {
+        sRegisteredContacts.setTables(CyncDBContract.ServerContactsEntry.TABLE_NAME + " LEFT JOIN " +
+                CyncDBContract.ContactsEntry.TABLE_NAME + " ON " +
+                CyncDBContract.ServerContactsEntry.COLUMN_NUMBER + " = " + CyncDBContract.ContactsEntry.COLUMN_NUMBER);
     }
 
-    BackgroundWebServerRequest(String oldContact, String paramString, Context paramContext)
-    {
+    BackgroundWebServerRequest(String oldContact, String paramString, Context paramContext) {
         this.mContext = paramContext;
         this.updatedNumber = paramString;
         this.oldContact = oldContact;
     }
 
-    private void sendUpdates()
-    {
+    private void sendUpdates() {
         SQLiteDatabase localSQLiteDatabase = new CyncDBHelper(this.mContext).getReadableDatabase();
-        Cursor localCursor = sRegisteredContacts.query(localSQLiteDatabase, new String[] { "IP" }, null, null, null, null, null);
-        while (localCursor.moveToNext())
-        {
+        Cursor localCursor = sRegisteredContacts.query(localSQLiteDatabase, new String[]{"IP"}, null, null, null, null, null);
+        while (localCursor.moveToNext()) {
             String IP = localCursor.getString(localCursor.getColumnIndex(CyncDBContract.ServerContactsEntry.COLUMN_IP));
             new CyncWebServiceClient(mContext, oldContact, updatedNumber, IP).init();
         }
@@ -45,8 +40,8 @@ public class BackgroundWebServerRequest
         localSQLiteDatabase.close();
     }
 
-    protected Void doInBackground(Void... paramVarArgs)
-    {
+
+    protected Void doInBackground(Void... paramVarArgs) {
         sendUpdates();
         return null;
     }
