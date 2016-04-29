543a6c94b487aa8dc94e06a7bc513ee036886483
Kamal <panapakam.kamaleshwar@gmail.com>
2016-04-29 03:18:36 -0400

    Removed the implicit ArrayAdapter and added a custom one.

--- app/src/main/java/com/restws/cync/kamal/cync/DetailsActivityFragment.java
+++ app/src/main/java/com/restws/cync/kamal/cync/DetailsActivityFragment.java
@@ -1,6 +1,9 @@
 package com.restws.cync.kamal.cync;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
@@ -10,7 +13,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,10 +32,11 @@ public class DetailsActivityFragment extends Fragment implements LoaderManager.L
 
     static Context mContext;
     static ArrayList<String> queryList;
-    static ArrayAdapter adp;
+    static QueryContactAdapter arrayAdapter;
     static ListView mQueryListView;
     static LoaderManager dummyActivity;
 
+    private static final String SELECTED_KEY = "selected_position";
     private int mPosition = ListView.INVALID_POSITION;
     private static final int CONTACT_QUERY_LOADER = 81001;
     public static LoaderManager.LoaderCallbacks<List<String>> loaderContext;
@@ -41,11 +47,12 @@ public class DetailsActivityFragment extends Fragment implements LoaderManager.L
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DetailsActivityFragment.mContext = getActivity().getApplicationContext();
+        DetailsActivityFragment.mContext = getActivity();
         loaderContext = this;
         dummyActivity = getActivity().getSupportLoaderManager();
         queryList = new ArrayList<>();
-        adp = new ArrayAdapter(mContext, R.layout.query_list_view_elem_details, new ArrayList());
+        arrayAdapter = new QueryContactAdapter(mContext, R.layout.query_list_view_elem_details, new ArrayList());
+
     }
 
 
@@ -55,8 +62,8 @@ public class DetailsActivityFragment extends Fragment implements LoaderManager.L
         View rootView = inflater.inflate(R.layout.fragment_details, container, false);
         getActivity().getSupportLoaderManager().initLoader(CONTACT_QUERY_LOADER, null, this);
         mQueryListView = (ListView) rootView.findViewById(R.id.query_list_view);
-        mQueryListView.setAdapter(adp);
-        adp.notifyDataSetChanged();
+        mQueryListView.setAdapter(arrayAdapter);
+        arrayAdapter.notifyDataSetChanged();
         return rootView;
     }
 
@@ -93,12 +100,82 @@ public class DetailsActivityFragment extends Fragment implements LoaderManager.L
         if (mPosition != ListView.INVALID_POSITION) {
             mQueryListView.smoothScrollToPosition(mPosition);
         }
-        adp.addAll(data);
+        arrayAdapter.addAll(data);
     }
 
     @Override
     public void onLoaderReset(Loader<List<String>> loader) {
-        adp.clear();
+        arrayAdapter.clear();
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (mPosition != ListView.INVALID_POSITION) {
+            outState.putInt(SELECTED_KEY, mPosition);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+
+    public static class QueryContactAdapter extends ArrayAdapter<String> {
+
+        private Context mContext;
+        private List<String> mData;
+
+        public QueryContactAdapter(Context context, int resource, List<String> data) {
+            super(context, resource, data);
+            this.mContext = context;
+            this.mData = data;
+        }
+
+        public static class ViewHolder {
+            public TextView contactName;
+            public TextView contactNumber;
+            public Button query_call;
+        }
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            final ViewHolder v_holder;
+
+            if (convertView == null) {
+                LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
+                convertView = inflater.inflate(R.layout.query_list_view_elem_details, parent, false);
+
+                v_holder = new ViewHolder();
+                v_holder.contactName = (TextView) convertView.findViewById(R.id.q_contact_name_text_view);
+                v_holder.contactNumber = (TextView) convertView.findViewById(R.id.q_contact_number_text_view);
+                v_holder.query_call = (Button) convertView.findViewById(R.id.query_list_item_call);
+
+                convertView.setTag(v_holder);
+            } else {
+                v_holder = (ViewHolder) convertView.getTag();
+            }
+            String contact = mData.get(position);
+            String name = contact.split(":")[0];
+            String number = contact.split(":")[1];
+            v_holder.contactName.setText(name);
+            v_holder.contactNumber.setText(number);
+
+            v_holder.query_call.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String phoneNumber = v_holder.contactNumber.getText().toString();
+                    String absNumber = phoneNumber.substring((phoneNumber.length() - 10), phoneNumber.length());
+                    String callUri = "tel:" + absNumber.trim();
+                    Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                    callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    callIntent.setData(Uri.parse(callUri));
+                    mContext.getApplicationContext().startActivity(callIntent);
+                }
+            });
+
+            return convertView;
+        }
+    }
+
+
 }
+
