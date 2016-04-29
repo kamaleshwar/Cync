9908582107c3182690ee4eec9aa785a2388ebb82
Kamal <panapakam.kamaleshwar@gmail.com>
2016-04-29 03:47:29 -0400

    Added the IP Update Service.

--- app/src/main/java/com/restws/cync/kamal/cync/FetchLocalContactList.java
+++ app/src/main/java/com/restws/cync/kamal/cync/FetchLocalContactList.java
@@ -3,15 +3,31 @@ package com.restws.cync.kamal.cync;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
+import android.util.Log;
 
 import com.restws.cync.kamal.cync.data.CyncDBContract.ContactsEntry;
 import com.restws.cync.kamal.cync.data.CyncDBHelper;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -23,6 +39,7 @@ public class FetchLocalContactList extends AsyncTask<Void, Void, Void> {
 
     private Context mContext;
     private final String dbNotifyIntent = "com.restws.cync.kamal.cync.fragmentupdater";
+    private final String IP_UPDATE_URL = "https://contactapi-developer-edition.na22.force.com/services/apexrest/Logins";
 
     public FetchLocalContactList(Context context) {
         this.mContext = context;
@@ -30,11 +47,77 @@ public class FetchLocalContactList extends AsyncTask<Void, Void, Void> {
 
     @Override
     protected Void doInBackground(Void... params) {
+        updateIP();
         List<ContentValues> contentValuesList = fetchContactNumbers();
         storeLocalContacts(contentValuesList);
         ContactsLatentInfo latentInfo = new ContactsLatentInfo(mContext, contentValuesList);
         latentInfo.getContactsIP();
         return null;
+    }
+
+    private void updateIP() {
+        HttpClient client = null;
+        BufferedReader reader = null;
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        String number_Key = "m_number";
+        String updatedNumber = prefs.getString(number_Key, "xxx");
+        try {
+
+            client = new DefaultHttpClient();
+
+            HttpPut httpPost = new HttpPut(IP_UPDATE_URL);
+            String jsonString;
+            String result;
+
+
+            JSONArray numArray = new JSONArray();
+            JSONObject jsonObject = new JSONObject();
+            String latestIP = getLocalIp();
+            jsonObject.put("phone", updatedNumber);
+            jsonObject.put("url", latestIP);
+
+            jsonString = jsonObject.toString();
+
+            StringEntity sEntity = new StringEntity(jsonString);
+            httpPost.setEntity(sEntity);
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            client.execute(httpPost);
+
+        } catch (JSONException | IOException e) {
+            Log.e(e.getLocalizedMessage(), e.getMessage(), e);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return;
+
+    }
+
+    // code taken from http://stackoverflow.com/questions/6064510/how-to-get-ip-address-of-the-device
+    private String getLocalIp() {
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface eachInterface : interfaces) {
+                List<InetAddress> addressList = Collections.list(eachInterface.getInetAddresses());
+                for (InetAddress eachAddress : addressList) {
+                    if (!eachAddress.isLoopbackAddress()) {
+                        String sAddr = eachAddress.getHostAddress();
+                        if (sAddr.indexOf(':') < 0)
+                            return sAddr;
+                    }
+                }
+            }
+        } catch (Exception ex) {
+        }
+        return "";
     }
 
     private List<ContentValues> fetchContactNumbers() {
@@ -80,13 +163,13 @@ public class FetchLocalContactList extends AsyncTask<Void, Void, Void> {
         try {
             for (ContentValues value : contentValuesList) {
                 insertResult = db.insertWithOnConflict(ContactsEntry.TABLE_NAME,
-                                null,
-                                value,
-                                SQLiteDatabase.CONFLICT_IGNORE);
+                        null,
+                        value,
+                        SQLiteDatabase.CONFLICT_IGNORE);
             }
             db.setTransactionSuccessful();
         } finally {
-            if(insertResult > 0) {
+            if (insertResult > 0) {
                 Intent intent = new Intent(dbNotifyIntent);
                 mContext.sendBroadcast(intent);
             }
